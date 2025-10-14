2211298924839444aa7b45481e1384dba329060e
mdhasibseo <mdhasibseoexpert@gmail.com>
2025-10-14 22:52:00 +0600

    cash-out-js

---
+++ cashout.js
@@ -0,0 +1,28 @@
+
+
+document.getElementById("cash-out-from").style.display = "none";
+
+document.getElementById("cash-out").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("cash-out-from").style.display = "block";
+    document.getElementById("last-bill").style.display = "none";
+    document.getElementById("withdraw-btn").addEventListener("click",function(event){
+        event.preventDefault();
+        const amount = document.getElementById("cash-out-amount");
+    const convertedAmount = parseFloat(amount.value);
+    const mainAmount = document.getElementById("main-a");
+    const convertedMainAmount = parseFloat(mainAmount.innerText);
+    const pin = document.getElementById("withdraw-pin").value;
+    const convertedPin = parseInt(pin)
+
+   if (convertedPin === 1234){
+    const final = convertedMainAmount - convertedAmount;
+    mainAmount.innerText = final
+   }
+   else{
+    alert("1234 pin dao ")
+   }
+
+    })
+    
+})
